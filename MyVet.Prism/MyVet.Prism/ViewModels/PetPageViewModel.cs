afa297927c0d65ab64297bd5721656f298b357f0
Adrian Soto Adame <00276291@red.unid.mx>
2020-09-02 17:48:52 -0500

    Cambios y agregados

---
+++ MyVet.Prism/MyVet.Prism/ViewModels/PetPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MyVet.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyVet.Prism.ViewModels
+{
+    public class PetPageViewModel : ViewModelBase
+    {
+        private PetResponse _pet;
+
+        public PetPageViewModel(INavigationService navigationService): base(navigationService)
+        {
+            
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if (parameters.ContainsKey("pet"))
+            {
+                _pet = parameters.GetValue<PetResponse>("pet");
+                Title = _pet.Name;
+            }
+        }
+    }
+}
